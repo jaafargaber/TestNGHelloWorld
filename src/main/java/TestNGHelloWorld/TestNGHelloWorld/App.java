d935b4685d9eb3d8b424ef5cbd6ebcd1dcb2d449
gaber <jaafar.gaber@gmail.com>
2021-01-04 19:13:51 +0100

    commit 2

---
+++ src/main/java/TestNGHelloWorld/TestNGHelloWorld/App.java
@@ -0,0 +1,13 @@
+package TestNGHelloWorld.TestNGHelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
